40eff606699e53854ad8de961a2a6960da68e171
Izaaq <60147050+Izaaq@users.noreply.github.com>
2022-03-21 18:12:20 +0000

    Update lolcode-shell.py

--- lolcode-shell.py
+++ lolcode-shell.py
@@ -1,16 +1,15 @@
 """
 LOLCODE shell - To be run for code to work.
-
 Izaaq bin Ahmad Izham - k19011071
 """
-
 from lol.lexer import LexerLOL
 from lol.parser import ParserLOL
 from lol.interpreter import InterpreterLOL
 import os
+import time
 
 """
-LOLCODE file reader - file must exist in 'tests' directory and end in '.lolcode'.
+LOLCODE file reader - file must exist in 'tests' directory and end in '.lol'.
 """
 if __name__ == '__main__':
     lexer = LexerLOL()
@@ -41,9 +40,12 @@ if __name__ == '__main__':
         raise Exception("EOF error")
 
     if lines:
+        startTime = time.perf_counter()    #
         lex = lexer.tokenize(''.join(lines))
         # for token in lex:       # debugging - show all tokens
         #     print(token)
         tree = parser.parse(lex)
         InterpreterLOL(tree, env)
-        # print(env)              # debugging - show all variables/functions
+        endTime = time.perf_counter()
+        print(f"Time taken: {endTime - startTime} seconds.")        # to measure time
+        print(env)              # debugging - show all variables/functions
